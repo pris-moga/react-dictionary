e7f827636ad95139747e7f3fae2fa93a266cab12
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-20 15:28:31 -0600

    Added Color Palette

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
-    <div className="Menaing">
+    <div className="Menaing mt-5">
       <h5>{props.meaning.partOfSpeech}</h5>
       {props.meaning.definitions.map(function (definition, index) {
         return (
--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h3>{props.results.word}</h3>
+        <h3 className="mt-5">{props.results.word}</h3>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
