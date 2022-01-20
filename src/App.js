aa9a7d570780fb58b14ad3fe900f6329f7b66b47
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-19 23:50:16 -0600

    Dictionary project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lexicon</h1>
+      <br />
+      <header className="App-header">Hi I'm a Dictionary in process</header>
+    </div>
+  );
+}
+
+export default App;
