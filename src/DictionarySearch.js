7919407f6e855c4723b800e217f676fc24bbd841
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-20 13:41:37 -0600

    Added Dictionary API integration

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    // Dictionary API documentation - https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
