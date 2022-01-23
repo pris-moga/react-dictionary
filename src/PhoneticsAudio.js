c64b6eae773d2a5770a4debedb983a4dcef1fc63
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-23 15:04:14 -0600

    Update PhoneticsAudio.js

--- src/PhoneticsAudio.js
+++ src/PhoneticsAudio.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function PhoneticsAudio(props) {
   if (props.audio) {
     return (
-      <a href={props.audio} target="_blank">
+      <a href={props.audio} target="_blank" rel="noreferrer">
         <i className="fas fa-volume-off"></i>
       </a>
     );
