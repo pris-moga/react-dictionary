387f6fa6a50246d08cf7b5835274c12a5a571948
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-26 22:38:21 -0600

    Finished styling the app

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Example from "./Example";
 import Synonyms from "./Synonyms";
 
 import "./Meaning.css";
@@ -6,16 +7,15 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h5>{props.meaning.partOfSpeech}</h5>
+      <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p className="uppercase mt-3">{definition.definition}</p>
-            <p>
-              <small>
-                <em>Example: {definition.example}</em>
-              </small>
+            <p className="uppercase mt-3">
+              <span className="definition">Definition: </span>
+              {definition.definition}
             </p>
+            <Example example={definition.example} />
             <Synonyms synonyms={definition.synonyms} />
           </div>
         );
