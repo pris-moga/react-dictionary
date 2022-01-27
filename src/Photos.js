e977e5abeabc02c186b7d6ca15af59e4dc7916a8
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-27 17:58:12 -0600

    SEO Checkout

--- src/Photos.js
+++ src/Photos.js
@@ -10,8 +10,17 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div key={index} className="col-sm-2 col-md-4 mb-3">
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" alt="" />{" "}
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noreferrer"
+                  title="Cick to open"
+                >
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
                 </a>
               </div>
             );
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -7,7 +7,11 @@ export default function Synonyms(props) {
     return (
       <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym} / </span>;
+          return (
+            <span key={index}>
+              {synonym} <span className="division">/</span>{" "}
+            </span>
+          );
         })}
       </div>
     );
